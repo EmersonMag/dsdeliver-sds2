e5f7b6050025c58c0dbeb610beeb17c5d93e6e6e
Emerson Magalhães <78791352+EmersonMag@users.noreply.github.com>
2022-02-22 18:33:43 -0300

    Update Routess.tsx

--- front-web/src/Routess.tsx
+++ front-web/src/Routess.tsx
@@ -1,18 +1,24 @@
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
-import Home from './Home';
-import Navbar from './Navbar';
-import Orders from './Orders';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Orders from "./Orders";
+import Navbar from "./Navbar";
 
-function Routess() {
-  return (
-    <BrowserRouter>
-    <Navbar />
+function Routess(){
+    return(
+        <BrowserRouter>
+                 <Navbar />
+            <Switch>
+                <Route path="/orders">
+                    <Orders />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
 
-      <Routes>
-        <Route path="/" element={<Home />}> </Route>
-        <Route path="orders" element={<Orders />} />
-      </Routes>
-
-    </BrowserRouter>
-  );
 }
+
+export default Routess;
+
